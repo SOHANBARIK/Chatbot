848184f28a63e02941b547950c9b2459cb823633
SOHANBARIK <162982656+SOHANBARIK@users.noreply.github.com>
2024-08-03 01:14:11 +0530

    Create url_data.py
    all data in a url used for chatbot

---
+++ url_data.py
@@ -0,0 +1,9 @@
+from chatbot import data
+from chatbot import text
+from chatbot import sentences
+from chatbot import sentence_list
+
+#For all url text
+print(data(text))
+#For all url text in form of list
+print(sentences(sentence_list))
